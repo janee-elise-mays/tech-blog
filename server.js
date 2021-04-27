c2ae543f4ffd85877b7c24b93fb6f7d745fcd83e
Janeé Mays <janee.elise@gmail.com>
2021-04-27 13:40:28 -0400

    added server.js and blog-routes.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/blog-routes'));
+
+app.listen(PORT, () => {
+    console.log('server listening on: http://localhost:' + PORT);
+});
