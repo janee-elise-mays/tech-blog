e0a595c0527566020019dea4d861b8d0b674e717
Janeé Mays <janee.elise@gmail.com>
2021-05-01 15:14:32 -0400

    added a home-routes.js file and added to index.js controllers file

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes')
 
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
+router.use('/api', homeRoutes);
 
 module.exports = router;
