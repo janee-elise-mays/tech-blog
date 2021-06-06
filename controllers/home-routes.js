03f2216fe68bf4e0197f6859b65a4f5443e9ebea
Janeé Mays <janee.elise@gmail.com>
2021-06-06 19:52:15 -0400

    revision to login home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,43 +32,33 @@ router.get('/post/:id', async (req, res) => {
     if (postData) {
     const post = postData.get({ plain: true });
 
-    res.render('post', {
-      ...post,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/dashboard', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('dashboard', {
-      ...user,
-      logged_in: true
-    });
+    res.render('post', { post });
+    } else {
+      res.status(404).end();
+    }
+  
   } catch (err) {
     res.status(500).json(err.message);
   }
 });
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/dashboard');
+  if (req.session.loggedin) {
+    res.redirect('/');
+    return
+    }
+    
+    res.render('login')
+  });
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
     return;
   }
 
-  res.render('login');
+  res.render('signup');
 });
 
 module.exports = router;
+
