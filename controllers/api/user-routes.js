29e14e92fc598a0c3a67b9a880bceaccd6e9b301
Janeé Mays <janee.elise@gmail.com>
2021-06-07 19:24:47 -0400

    user-routes update

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -22,20 +22,20 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ 
+    const user = await User.findOne({ 
       where: { 
         username: req.body.username, 
       },
    });
 
-    if (!userData) {
+    if (!user) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    const validPassword = await user.checkPassword(req.body.password);
+    const validPassword = user.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
